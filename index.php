73d4a86a58a58981134e07a0e41e5954d834ae46
erisjimver <erisjinver@gmail.com>
2019-08-13 21:58:44 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Base de datos</title>
+</head>
+<body>
+
+<?php
+
+
+	$db_host="localhost";
+	$db_nombre="pruebas";
+	$db_usuario="root";
+	$db_contra="";
+
+	$conexion=mysqli_connect($db_host,$db_usuario,$db_contra,$db_nombre);
+
+	$consulta="select * from datos_personales";
+
+	$resultado=mysqli_query($conexion,$consulta);
+
+	$fila=mysqli_fetch_row($resultado);
+
+	echo $fila[0]. " ";
+	echo $fila[1]. " ";
+	echo $fila[2]. " ";
+	echo $fila[3]. " ";
+
+?>
+</body>
+</html>
