59cb2828f862d5a7a5a1abd228d0de139c4ba471
erisjimver <erisjinver@gmail.com>
2019-08-14 20:52:47 -0500

    consulta por busqueda like en hojas separadas

---
+++ index2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Busqueda</title>
+</head>
+<body>
+
+<form action="pagina_busqueda.php" method="get">
+	 
+	<label>Buscar:<input type="text" name="buscar"></label>
+
+	<input type="submit" name="enviando" value="Dale!">
+
+</form>
+
+
+</body>
+</html>
