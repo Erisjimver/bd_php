f3081572354717f021f33ce20886ede997b92133
erisjimver <erisjinver@gmail.com>
2019-08-18 23:45:27 -0500

    inyecion sql delete

--- formulario_login_baja.php
+++ formulario_login_baja.php
@@ -5,11 +5,11 @@
 </head>
 <body>
 
-<form action="pagina_datos.php" method="get">
+<form action="pagina_datos_baja.php" method="get">
 	 
 	<label>Buscar:<input type="text" name="usu"></label>
 	<label>Contraseña:<input type="text" name="con"></label>
-	<input type="submit" name="enviando" value="Login!">
+	<input type="submit" name="enviando" value="Dar Baja!">
 
 </form>
 
---
+++ pagina_datos_baja.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Base de datos</title>
+</head>
+<body>
+
+<?php
+
+	$usuario=$_GET["usu"];
+	$contra=$_GET["con"];
+
+	require("datos_conexion.php");
+
+	$conexion=mysqli_connect($db_host,$db_usuario,$db_contra);
+
+	if(mysqli_connect_errno()){
+		echo "Error de  conexion a bd";
+		exit();
+	}
+	//else{
+
+	mysqli_select_db($conexion,$db_nombre) or die("No se encuentra la base de datos");
+
+	mysqli_set_charset($conexion,"utf8");//linea que permite mostrar tildes en las consultas de la bd
+
+
+	//$consulta="select * from usuarios where usuario='$usuario' and contra='$contra'";
+
+
+	$consulta="delete from usuarios where usuario='$usuario' and contra='$contra'";
+
+	echo "$consulta <br><br>";
+
+	if(mysqli_query($conexion,$consulta)){
+		echo "Baja Procesada";
+	}
+	/*else{
+		echo "No se ha ejecutado la baja correctamente";
+	}
+*/
+
+	mysqli_close($conexion);//cerrar conecion
+?>
+</body>
+</html>
+
+
+
